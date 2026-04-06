db4e3ab3bcf6c5f43cebb4daca8c4a3a6325a0e6
Eike  Lurz <eike.lurz@tuta.io>
2026-04-06 20:19:48 +0200

    ♻️ Switch from eslint to biome (#968)

--- src/CalledWithFn.ts
+++ src/CalledWithFn.ts
@@ -1,28 +1,30 @@
-import { CalledWithMock } from './Mock'
-import { Matcher, MatchersOrLiterals } from './Matchers'
-import { FallbackImplementation } from './types'
-import { vi, Mock } from 'vitest'
+import { type Mock, vi } from 'vitest'
+import { Matcher, type MatchersOrLiterals } from './Matchers'
+import type { CalledWithMock } from './Mock'
+import type { CalledWithImplementation, FallbackImplementation } from './types'
 
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
 interface CalledWithStackItem<T, Y extends any[]> {
   args: MatchersOrLiterals<Y>
   calledWithFn: Mock<FallbackImplementation<Y, T>>
 }
 
 interface VitestAsymmetricMatcher {
+  // biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
   asymmetricMatch(...args: any[]): boolean
 }
 
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
 function isVitestAsymmetricMatcher(obj: any): obj is VitestAsymmetricMatcher {
-  return !!obj && typeof obj === 'object'
-    && 'asymmetricMatch' in obj
-    && typeof obj.asymmetricMatch === 'function'
+  return !!obj && typeof obj === 'object' && 'asymmetricMatch' in obj && typeof obj.asymmetricMatch === 'function'
 }
 
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
 const checkCalledWith = <T, Y extends any[]>(
   calledWithStack: CalledWithStackItem<T, Y>[],
   actualArgs: Y,
-  fallbackMockImplementation?: FallbackImplementation<Y, T>,
-): T => {
+  fallbackMockImplementation?: FallbackImplementation<Y, T>
+): T | undefined => {
   const calledWithInstance = calledWithStack.find((instance) =>
     instance.args.every((matcher, i) => {
       if (matcher instanceof Matcher) {
@@ -34,39 +36,46 @@ const checkCalledWith = <T, Y extends any[]>(
       }
 
       return actualArgs[i] === matcher
-    }),
+    })
   )
 
-  // @ts-ignore
   return calledWithInstance
     ? calledWithInstance.calledWithFn(...actualArgs)
-    : fallbackMockImplementation && fallbackMockImplementation(...actualArgs)
+    : fallbackMockImplementation?.(...actualArgs)
 }
 
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
 type CalledWithFnArgs<Y extends any[], T> = { fallbackMockImplementation?: FallbackImplementation<Y, T> }
 
-const calledWithFn = <T, Y extends any[]>({ fallbackMockImplementation }: CalledWithFnArgs<Y, T> = {}): CalledWithMock<T, Y> => {
-  const fn: Mock<FallbackImplementation<Y, T>> = fallbackMockImplementation ? vi.fn(fallbackMockImplementation) : vi.fn()
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any matcher that has an asymmetricMatch function, not just our own Matcher class.
+const calledWithFn = <T, Y extends any[]>({
+  fallbackMockImplementation,
+}: CalledWithFnArgs<Y, T> = {}): CalledWithMock<T, Y> => {
+  const fn: Mock<CalledWithImplementation<Y, T>> = fallbackMockImplementation
+    ? vi.fn(fallbackMockImplementation)
+    : vi.fn()
   let calledWithStack: CalledWithStackItem<T, Y>[] = []
 
-    ; (fn as CalledWithMock<T, Y>).calledWith = (...args) => {
-      // We create new function to delegate any interactions (mockReturnValue etc.) to for this set of args.
-      // If that set of args is matched, we just call that vi.fn() for the result.
-      const calledWithFn: Mock<FallbackImplementation<Y, T>> = fallbackMockImplementation ? vi.fn(fallbackMockImplementation) : vi.fn()
-      const mockImplementation = fn.getMockImplementation()
-      if (
-        !mockImplementation ||
-        fn.getMockImplementation()?.name === 'implementation' ||
-        mockImplementation === fallbackMockImplementation
-      ) {
-        // Our original function gets a mock implementation which handles the matching
-        fn.mockImplementation((...args: Y) => checkCalledWith(calledWithStack, args, fallbackMockImplementation))
-        calledWithStack = []
-      }
-      calledWithStack.unshift({ args, calledWithFn })
-
-      return calledWithFn
+  ;(fn as CalledWithMock<T, Y>).calledWith = (...args) => {
+    // We create new function to delegate any interactions (mockReturnValue etc.) to for this set of args.
+    // If that set of args is matched, we just call that vi.fn() for the result.
+    const calledWithFn: Mock<FallbackImplementation<Y, T>> = fallbackMockImplementation
+      ? vi.fn(fallbackMockImplementation)
+      : vi.fn()
+    const mockImplementation = fn.getMockImplementation()
+    if (
+      !mockImplementation ||
+      fn.getMockImplementation()?.name === 'implementation' ||
+      mockImplementation === fallbackMockImplementation
+    ) {
+      // Our original function gets a mock implementation which handles the matching
+      fn.mockImplementation((...args: Y) => checkCalledWith(calledWithStack, args, fallbackMockImplementation))
+      calledWithStack = []
     }
+    calledWithStack.unshift({ args, calledWithFn })
+
+    return calledWithFn
+  }
 
   return fn as CalledWithMock<T, Y>
 }
--- src/Matchers.ts
+++ src/Matchers.ts
@@ -1,7 +1,6 @@
 type MatcherFn<T> = (actualValue: T) => boolean
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface MatcherLike<T> {
+interface MatcherLike<_> {
   asymmetricMatch(other: unknown): boolean
   toString(): string
   getExpectedType?(): string
@@ -15,7 +14,7 @@ class Matcher<T> implements MatcherLike<T> {
 
   constructor(
     readonly asymmetricMatch: MatcherFn<T>,
-    private readonly description: string,
+    private readonly description: string
   ) {
     this.$$typeof = Symbol.for('vi.asymmetricMatcher')
   }
@@ -62,26 +61,30 @@ class CaptorMatcher<T> {
   }
 }
 
-interface MatcherCreator<T, E = T> {
-  (expectedValue?: E): Matcher<T>
-}
+type MatcherCreator<T, E = T> = (expectedValue?: E) => Matcher<T>
 
 type MatchersOrLiterals<Y extends unknown[]> = { [K in keyof Y]: MatcherLike<Y[K]> | Y[K] }
 
 const any: MatcherCreator<unknown> = () => new Matcher(() => true, 'any()')
-const anyBoolean: MatcherCreator<boolean> = () => new Matcher((actualValue: boolean) => typeof actualValue === 'boolean', 'anyBoolean()')
+const anyBoolean: MatcherCreator<boolean> = () =>
+  new Matcher((actualValue: boolean) => typeof actualValue === 'boolean', 'anyBoolean()')
 const anyNumber: MatcherCreator<number> = () =>
-  new Matcher((actualValue) => typeof actualValue === 'number' && !isNaN(actualValue), 'anyNumber()')
-const anyString: MatcherCreator<string> = () => new Matcher((actualValue: string) => typeof actualValue === 'string', 'anyString()')
+  new Matcher((actualValue) => typeof actualValue === 'number' && !Number.isNaN(actualValue), 'anyNumber()')
+const anyString: MatcherCreator<string> = () =>
+  new Matcher((actualValue: string) => typeof actualValue === 'string', 'anyString()')
 const anyFunction: MatcherCreator<CallableFunction> = () =>
   new Matcher((actualValue: CallableFunction) => typeof actualValue === 'function', 'anyFunction()')
-const anySymbol: MatcherCreator<symbol> = () => new Matcher((actualValue) => typeof actualValue === 'symbol', 'anySymbol()')
+const anySymbol: MatcherCreator<symbol> = () =>
+  new Matcher((actualValue) => typeof actualValue === 'symbol', 'anySymbol()')
 const anyObject: MatcherCreator<unknown> = () =>
   new Matcher((actualValue) => typeof actualValue === 'object' && actualValue !== null, 'anyObject()')
 
 const anyArray: MatcherCreator<unknown[]> = () => new Matcher((actualValue) => Array.isArray(actualValue), 'anyArray()')
-const anyMap: MatcherCreator<Map<unknown, unknown>> = () => new Matcher((actualValue) => actualValue instanceof Map, 'anyMap()')
+const anyMap: MatcherCreator<Map<unknown, unknown>> = () =>
+  new Matcher((actualValue) => actualValue instanceof Map, 'anyMap()')
 const anySet: MatcherCreator<Set<unknown>> = () => new Matcher((actualValue) => actualValue instanceof Set, 'anySet()')
+
+// biome-ignore lint/suspicious/noExplicitAny: This is necessary to support any class, not just ones that extend a specific base class.
 const isA: MatcherCreator<any> = (clazz) => new Matcher((actualValue) => actualValue instanceof clazz, 'isA()')
 
 const arrayIncludes: MatcherCreator<unknown[], unknown> = (arrayVal) =>
@@ -91,44 +94,49 @@ const setHas: MatcherCreator<Set<unknown>, unknown> = (arrayVal) =>
 const mapHas: MatcherCreator<Map<unknown, unknown>, unknown> = (mapVal) =>
   new Matcher((actualValue) => anyMap().asymmetricMatch(actualValue) && actualValue.has(mapVal), 'mapHas()')
 const objectContainsKey: MatcherCreator<Record<string, unknown>, string> = (key) =>
-  new Matcher((actualValue) => anyObject().asymmetricMatch(actualValue) && actualValue[key!] !== undefined, 'objectContainsKey()')
+  new Matcher(
+    // biome-ignore lint/style/noNonNullAssertion: We know key is defined since it's a parameter, but the type definition for asymmetricMatch requires us to check it at runtime.
+    (actualValue) => anyObject().asymmetricMatch(actualValue) && actualValue[key!] !== undefined,
+    'objectContainsKey()'
+  )
 const objectContainsValue: MatcherCreator<Record<string, unknown> | ArrayLike<unknown>> = (value) =>
   new Matcher(
     (actualValue) => anyObject().asymmetricMatch(actualValue) && Object.values(actualValue).includes(value),
-    'objectContainsValue()',
+    'objectContainsValue()'
   )
 
 const notNull: MatcherCreator<unknown> = () => new Matcher((actualValue) => actualValue !== null, 'notNull()')
-const notUndefined: MatcherCreator<unknown> = () => new Matcher((actualValue) => actualValue !== undefined, 'notUndefined()')
+const notUndefined: MatcherCreator<unknown> = () =>
+  new Matcher((actualValue) => actualValue !== undefined, 'notUndefined()')
 const notEmpty: MatcherCreator<unknown> = () =>
   new Matcher((actualValue) => actualValue !== null && actualValue !== undefined && actualValue !== '', 'notEmpty()')
 
 const captor = <T>() => new CaptorMatcher<T>()
 const matches = <T>(matcher: MatcherFn<T>) => new Matcher(matcher, 'matches()')
 
+export type { MatcherCreator, MatcherFn, MatcherLike, MatchersOrLiterals }
 export {
-  Matcher,
-  CaptorMatcher,
   any,
-  anyBoolean,
-  anyNumber,
-  anyString,
-  anyFunction,
-  anySymbol,
-  anyObject,
   anyArray,
+  anyBoolean,
+  anyFunction,
   anyMap,
+  anyNumber,
+  anyObject,
   anySet,
-  isA,
+  anyString,
+  anySymbol,
   arrayIncludes,
-  setHas,
+  CaptorMatcher,
+  captor,
+  isA,
+  Matcher,
   mapHas,
-  objectContainsKey,
-  objectContainsValue,
+  matches,
+  notEmpty,
   notNull,
   notUndefined,
-  notEmpty,
-  captor,
-  matches,
+  objectContainsKey,
+  objectContainsValue,
+  setHas,
 }
-export type { MatcherFn, MatchersOrLiterals, MatcherCreator, MatcherLike }
